9901bdb5cba35292feb656cb8a2057ecba211511
engineer-rukya <g.rukijat.04@icloud.com>
2026-07-14 22:32:37 +0300

    Registered simple tag for categories

--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
-from app.settings import DEBUG
+from app import settings
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("main.urls", namespace="main")),
-    path("catalog/", include("goods.urls", namespace="catalog")),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
--- main/views.py
+++ main/views.py
@@ -5,11 +5,9 @@ from django.shortcuts import render
 from goods.models import Categories
 
 def index(request):
-    categories = Categories.objects.all()
     context = {
         'title': 'LL',
         'content': 'L library',
-        'categories': categories,
     }
 
     return render(request, 'main/index.html', context)
