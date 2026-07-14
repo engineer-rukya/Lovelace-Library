b196fc26f9c00c0bebfc069335c2af7693c1ff8d
engineer-rukya <g.rukijat.04@icloud.com>
2026-07-15 00:00:58 +0300

    Slug converter for product

--- goods/urls.py
+++ goods/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 
 from goods import views
 
-app_name = 'goods'
+app_name = "goods"
 
 urlpatterns = [
-    path('', views.catalog, name='index'),
-    path('product/', views.product, name='product'),
+    path("", views.catalog, name="index"),
+    path("product/<slug:product_slug>/", views.product, name="product"),
 ]
-
