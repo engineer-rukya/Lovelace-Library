23ac408c94eea684c5ff3a101f1cbf27583de61a
engineer-rukya <g.rukijat.04@icloud.com>
2026-07-15 07:35:11 +0300

    class based views

--- main/views.py
+++ main/views.py
@@ -1,23 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 
 from goods.models import Categories
 
-def index(request):
-    context = {
-        'title': 'LL',
-        'content': 'L library',
-    }
-
-    return render(request, 'main/index.html', context)
 
 
-def about(request):
-    context = {
-        'title': 'LL - О нас',
-        'content': "О нас",
-        'text_on_page': "Текст о нас"
-    }
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
 
-    return render(request, 'main/about.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'LL - Главная'
+        context['content'] = "L Library"
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'LL - О нас'
+        context['content'] = "О нас"
+        context['text_on_page'] = "Текст о нас"
+        return context
+    
