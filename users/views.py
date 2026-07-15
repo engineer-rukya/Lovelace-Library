0214dd106f7b85d1bfac63a37629b22f502a7f88
engineer-rukya <g.rukijat.04@icloud.com>
2026-07-15 04:50:49 +0300

    Registration form and controller are ready

--- users/views.py
+++ users/views.py
@@ -1,44 +1,48 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = request.POST["username"]
+            password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('main:index'))
+                return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
 
-    context = {
-        'title': 'LL - Авторизация',
-        'form': form
-    }
-    return render(request, 'users/login.html', context)
+    context = {"title": "LL - Авторизация", "form": form}
+    return render(request, "users/login.html", context)
 
 
 def registration(request):
-    context = {
-        'title': 'LL - Регистрация'
-    }
-    return render(request, 'users/registration.html', context)
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserRegistrationForm()
+
+    context = {"title": "LL - Регистрация", "form": form}
+    return render(request, "users/registration.html", context)
 
 
 def profile(request):
-    context = {
-        'title': 'LL - Кабинет'
-    }
-    return render(request, 'users/profile.html', context)
+    context = {"title": "LL - Кабинет"}
+    return render(request, "users/profile.html", context)
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse("main:index"))
