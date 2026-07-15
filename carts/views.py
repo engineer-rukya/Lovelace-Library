f33fe770e9f8df0e7eaa7287e8afe898491ec7dc
engineer-rukya <g.rukijat.04@icloud.com>
2026-07-15 05:55:57 +0300

    Sessions - carts for anonymous user

--- carts/views.py
+++ carts/views.py
@@ -12,7 +12,7 @@ def cart_add(request):
     product_id = request.POST.get("product_id")
 
     product = Products.objects.get(id=product_id)
-
+    
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
 
@@ -24,10 +24,22 @@ def cart_add(request):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
+    else:
+        carts = Cart.objects.filter(
+            session_key=request.session.session_key, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(
+                session_key=request.session.session_key, product=product, quantity=1)
+    
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
-    )
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
 
     response_data = {
         "message": "Товар добавлен в корзину",
@@ -35,7 +47,7 @@ def cart_add(request):
     }
 
     return JsonResponse(response_data)
-
+            
 
 def cart_change(request):
     cart_id = request.POST.get("cart_id")
@@ -49,8 +61,7 @@ def cart_change(request):
 
     cart = get_user_carts(request)
     cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": cart}, request=request
-    )
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
 
     response_data = {
         "message": "Количество изменено",
@@ -61,8 +72,9 @@ def cart_change(request):
     return JsonResponse(response_data)
 
 
-def cart_remove(request):
 
+def cart_remove(request):
+    
     cart_id = request.POST.get("cart_id")
 
     cart = Cart.objects.get(id=cart_id)
@@ -71,8 +83,7 @@ def cart_remove(request):
 
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
-        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
-    )
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
 
     response_data = {
         "message": "Товар удален",
--- users/views.py
+++ users/views.py
@@ -3,73 +3,90 @@ from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from carts.models import Cart
 
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 def login(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST["username"]
-            password = request.POST["password"]
+            username = request.POST['username']
+            password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
+
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
-                    return HttpResponseRedirect(request.POST.get("next"))
-                return HttpResponseRedirect(reverse("main:index"))
+                    return HttpResponseRedirect(request.POST.get('next'))
+                    
+                return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
 
-    context = {"title": "LL - Авторизация", "form": form}
-    return render(request, "users/login.html", context)
+    context = {
+        'title': 'Home - Авторизация',
+        'form': form
+    }
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
-            messages.success(
-                request,
-                f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт",
-            )
-            return HttpResponseRedirect(reverse("main:index"))
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+            messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
+            return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
-
-    context = {"title": "LL - Регистрация", "form": form}
-    return render(request, "users/registration.html", context)
-
+    
+    context = {
+        'title': 'Home - Регистрация',
+        'form': form
+    }
+    return render(request, 'users/registration.html', context)
 
 @login_required
 def profile(request):
-    if request.method == "POST":
-        form = ProfileForm(
-            data=request.POST, instance=request.user, files=request.FILES
-        )
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, "Профайл успешно обновлен")
-            return HttpResponseRedirect(reverse("user:profile"))
+            return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = ProfileForm(instance=request.user)
 
-    context = {"title": "LL - Кабинет", "form": form}
-    return render(request, "users/profile.html", context)
-
+    context = {
+        'title': 'Home - Кабинет',
+        'form': form
+    }
+    return render(request, 'users/profile.html', context)
 
 def users_cart(request):
-    return render(request, "users/users_cart.html")
+    return render(request, 'users/users_cart.html')
 
 
 @login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
-    return redirect(reverse("main:index"))
+    return redirect(reverse('main:index'))
